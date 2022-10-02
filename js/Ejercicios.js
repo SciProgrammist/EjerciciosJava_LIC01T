7b25b5d5dd181b53fa2d0cc8b370bca06abdbc88
MariaTorres0 <mariatorres11001@gmail.com>
2022-10-02 12:05:06 -0600

    Ejercicio 3

--- js/Ejercicios.js
+++ js/Ejercicios.js
@@ -1,3 +1,134 @@
+
+
+//JS del Ejercicio 3
+
+
+function validar(valido){
+            var nombres = document.querySelectorAll("#form input[type=text]");
+            var notas = document.querySelectorAll("#form input[type=number]");
+            
+
+            for (var i = 0, element; element = nombres[i++];) {
+               if(element.value==''){
+                  alert('Favor completar los campos de nombre.');
+                  valido = false;
+                  element.focus();
+                  return valido
+               }else{
+                  valido = true;
+               }
+             }
+            for (var i = 0, element; element = notas[i++];) { //Sacamos notas y almacenamos en array
+               if(element.value==''){
+                  alert('Favor completar los campos de notas.');
+                  valido = false;
+                  element.focus();
+                  return valido;
+               }else{
+                  if (element.value>=0 && element.value<=10){
+                  valido = true;
+                  }
+                  else{
+                  alert('La nota tiene que estar entre el rango de 0 y 10.');
+                     valido = false;
+                     element.focus();
+                     return valido;
+                  }
+               }   
+             }
+            return valido;
+         }
+         var cont = 0; 
+         function agregarFil(){ 
+            var tabla = document.getElementById("tabla");
+            var nFilas = document.querySelectorAll("#tabla tr").length;
+         
+             if (nFilas == 0) {
+                 nFilas = 1;
+             }
+             cont = nFilas;
+            var tabla = document.getElementsByTagName("tbody")[0] .insertRow((cont-1));
+            var col2 = tabla.insertCell(0);
+            var col3 = tabla.insertCell(1);
+            var col4 = tabla.insertCell(2);
+            var col5 = tabla.insertCell(3);
+            var col6 = tabla.insertCell(4);
+         
+            col2.innerHTML = '<input type="text" class="form-control" placeholder="Alumno" required="">';
+            col3.innerHTML = '<input type="number" class="form-control" placeholder="Nota 1" name="notaAl3" min="0" max="10">';
+            col4.innerHTML = '<input type="number" class="form-control" placeholder="Nota 2" name="notaAl3" min="0" max="10">';
+            col5.innerHTML = '<input type="number" class="form-control" placeholder="Nota 3" name="notaAl3" min="0" max="10">';
+            col6.innerHTML = '<button type="button"  class="btn btn-danger" id="elim' + cont + '" onclick="eliminarFila(this)"><i class="fas fa-times"></i></button>';
+         
+            cont++;
+         }
+         function eliminarFila(element) {
+             var fila = element.parentNode.parentNode;
+           
+             fila.remove();
+          }
+         function procesar() {
+             var valido = false;
+             if(!validar()){
+               return;
+             }
+             var nombres = document.querySelectorAll("#form input[type=text]");
+             var notas = document.querySelectorAll("#form input[type=number]");
+             var bodyTabla = document.getElementById("bodyPromedios");
+             var notasArr = [];
+             var arrNombres = [];
+             var matriz = [];
+             var notasFin = [];
+             var promedio = 0;
+             var fila = "";
+             var arrTemp = [];
+             var contaArra = 0;
+         
+             for (var i = 0, element; element = nombres[i++];) { //Sacamos los nombres digitados y almacenamos en array
+                 arrNombres.push(element.value);
+             }
+         
+             for (var i = 0, element; element = notas[i++];) { //Sacamos notas y almacenamos en array
+                 notasArr.push(element.value);
+             }
+         
+             for (i = 0; i < notasArr.length; i++){ //Cortamos dinamicamente el arreglo completo de notas (de 3 en 3) para meterlos a la matriz
+                arrTemp.push(notasArr[i]);
+                contaArra++;
+                if(contaArra == 3){
+                  matriz.push(arrTemp);
+                  contaArra = 0;
+                  arrTemp=[];
+                }
+             }
+         
+             for (x = 0; x < matriz.length; x++) { //Recorremos la matriz para sacar promedios
+                 promedio = 0;
+                 for (y = 0; y < matriz[x].length; y++) { //Sumamos las notas de cada alumno
+                     promedio += parseFloat(matriz[x][y]);
+                 }
+                 promedio = parseFloat(promedio / 3);
+                 notasFin.push(promedio.toFixed(2)); //Guardamos en arreglo para posteriormente imprimir
+             }
+           
+         
+             for (i = 0; i < notasFin.length; i++) { //Imprimimos el promedio por alumno en una tabla
+                 fila += '<tr  id="fila' + i + '">' +
+                       '<th scope="row">' + (i+1) + '</th>' +
+                       '<td>'+ arrNombres[i] +'</td>' +
+                       '<td>'+ notasFin[i] +'</td>' +
+                       '<td><button type="button" class="btn btn-danger" id="elim1" onclick="eliminarFila(this)">'+
+                       ' <i class="fas fa-times"></i></button> '+
+                       '</td>'+
+                       '</tr>';
+               bodyTabla.innerHTML = fila; 
+             }
+         }
+         function validarCampos(){
+         
+         }
+
+
 // JS del Ejercicio 2
 
 
